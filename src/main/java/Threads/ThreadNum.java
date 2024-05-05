8c3e051572696c0eb247223d06d3575f453ec9ac
ytalo <ytalo4869@gmail.com>
2024-05-05 16:30:24 -0300

    Exercicio pronto

--- src/main/java/Threads/ThreadNum.java
+++ src/main/java/Threads/ThreadNum.java
@@ -2,20 +2,17 @@ package Threads;
 
 public class ThreadNum extends Thread{
     private Object[] lista;
-    private int[] novaLista = new int[3];
 
     public ThreadNum(Object[] lista){
             this.lista = lista;
     }
 
     public void run(){
-        byte i = 0;
-        System.out.print(" Os numeros dessa lista são: ");
+        System.out.print("\nOs numeros dessa lista são: ");
+
         for( Object e : lista ){
             Class<?> tipo = e.getClass();
             if (tipo.getName().equals("java.lang.Integer")){
-                novaLista[i] = (int) e;
-                i ++;
                 System.out.print(e);
             }
         }
--- src/main/java/principal/Main.java
+++ src/main/java/principal/Main.java
@@ -9,9 +9,9 @@ public class Main {
         Object[] lista = {"a",1,"b",2,"c",3};
 
         ThreadLetras threadLetters = new ThreadLetras(lista);
-        threadLetters.start();
-        //Thread.sleep(1000);
         ThreadNum threadNumbers = new ThreadNum(lista);
+
+        threadLetters.start();
         threadNumbers.start();
 
     }
